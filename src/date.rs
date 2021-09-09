702d368e8072997bb7a270d70fcf6ceb3e676b6f
Josh Stoik <josh@blobfolio.com>
2021-09-09 12:17:39 -0700

    lint

--- src/date.rs
+++ src/date.rs
@@ -352,7 +352,7 @@ impl FmtUtc2k {
 	/// let fmt = FmtUtc2k::max();
 	/// assert_eq!(fmt.as_str(), "2099-12-31 23:59:59");
 	/// ```
-	pub fn as_str(&self) -> &str {
+	pub const fn as_str(&self) -> &str {
 		unsafe { std::str::from_utf8_unchecked(&self.0) }
 	}
 
