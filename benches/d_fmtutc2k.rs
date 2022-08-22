ddd565cde7359753a08d395dbea18187d8291d49
Josh Stoik <josh@blobfolio.com>
2022-08-22 13:42:45 -0700

    bump: brunch 0.3

--- benches/d_fmtutc2k.rs
+++ benches/d_fmtutc2k.rs
@@ -14,23 +14,23 @@ use utc2k::{
 
 
 benches!(
-	Bench::new("utc2k::FmtUtc2k", "try_from(valid datetime)")
-		.with(|| FmtUtc2k::try_from("2019-04-10 18:18:55")),
+	Bench::new("utc2k::FmtUtc2k::try_from(valid datetime)")
+		.run(|| FmtUtc2k::try_from("2019-04-10 18:18:55")),
 
-	Bench::new("utc2k::FmtUtc2k", "try_from(valid date)")
-		.with(|| FmtUtc2k::try_from("2019-04-10")),
+	Bench::new("utc2k::FmtUtc2k::try_from(valid date)")
+		.run(|| FmtUtc2k::try_from("2019-04-10")),
 
-	Bench::new("utc2k::FmtUtc2k", "from(Utc2k)")
-		.with_setup(Utc2k::new(2019, 4, 10, 18, 18, 55), FmtUtc2k::from),
+	Bench::new("utc2k::FmtUtc2k::from(Utc2k)")
+		.run_seeded(Utc2k::new(2019, 4, 10, 18, 18, 55), FmtUtc2k::from),
 
 	Bench::spacer(),
 
-	Bench::new("utc2k::FmtUtc2k", "to_string()")
-		.with_setup(FmtUtc2k::from(Utc2k::MAX_UNIXTIME), |x| x.to_string()),
+	Bench::new("utc2k::FmtUtc2k::to_string()")
+		.run_seeded(FmtUtc2k::from(Utc2k::MAX_UNIXTIME), |x| x.to_string()),
 
-	Bench::new("utc2k::FmtUtc2k", "to_rfc2822()")
-		.with_setup(FmtUtc2k::from(Utc2k::MAX_UNIXTIME), |x| x.to_rfc2822()),
+	Bench::new("utc2k::FmtUtc2k::to_rfc2822()")
+		.run_seeded(FmtUtc2k::from(Utc2k::MAX_UNIXTIME), |x| x.to_rfc2822()),
 
-	Bench::new("utc2k::FmtUtc2k", "to_rfc3339()")
-		.with_setup(FmtUtc2k::from(Utc2k::MAX_UNIXTIME), |x| x.to_rfc3339()),
+	Bench::new("utc2k::FmtUtc2k::to_rfc3339()")
+		.run_seeded(FmtUtc2k::from(Utc2k::MAX_UNIXTIME), |x| x.to_rfc3339()),
 );
