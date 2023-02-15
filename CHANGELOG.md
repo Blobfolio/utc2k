e218ca3e1fc49deea6476b1b4fd2f226d9ab5adb
Josh Stoik <josh@blobfolio.com>
2023-02-14 19:52:17 -0800

    misc: improve deserialize implementations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 
 
+## [0.5.15](https://github.com/Blobfolio/utc2k/releases/tag/v0.5.15) - TBD
+
+### Changed
+
+* Improve `Deserialize` handling
+
+
+
 ## [0.5.14](https://github.com/Blobfolio/utc2k/releases/tag/v0.5.14) - 2023-02-04
 
 ### Changed
--- src/serde.rs
+++ src/serde.rs
@@ -12,143 +12,7 @@ use serde::{
 	ser,
 	Serialize,
 };
-use std::{
-	marker::PhantomData,
-	fmt,
-};
-
-
-
-/// # Temporary Deserialization Value.
-///
-/// This is a stripped-down version of the private `Content` enum provided by
-/// `serde_derive`. It allows us to deserialize our structs from timestamps or
-/// datetime strings.
-enum Value<'de> {
-	Invalid,
-	Int(u32),
-	String(String),
-	Str(&'de str),
-	ByteBuf(Vec<u8>),
-	Bytes(&'de [u8]),
-}
-
-impl<'de> Value<'de> {
-	/// # As Integer.
-	const fn as_int(&self) -> Option<u32> {
-		if let Self::Int(x) = self { Some(*x) }
-		else { None }
-	}
-
-	/// # As String Slice.
-	fn as_str(&self) -> Option<&str> {
-		match *self {
-			Self::Str(x) => Some(x),
-			Self::String(ref x) => Some(x),
-			Self::Bytes(x) => std::str::from_utf8(x).ok(),
-			Self::ByteBuf(ref x) => std::str::from_utf8(x).ok(),
-			_ => None,
-		}
-	}
-}
-
-impl<'de> Deserialize<'de> for Value<'de> {
-	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-	where
-		D: de::Deserializer<'de>,
-	{
-		// Untagged and internally tagged enums are only supported in
-		// self-describing formats.
-		let visitor = ValueVisitor { value: PhantomData };
-		deserializer.deserialize_any(visitor)
-	}
-}
-
-/// # Deserialization Visitor.
-struct ValueVisitor<'de> {
-	value: PhantomData<Value<'de>>,
-}
-
-/// # Helper: Generated Trait Methods for Unsupported Types.
-///
-/// Most of the types we do not support have the same signatures.
-macro_rules! invalid_type {
-	($(($fn:ident, $ty:ty)),+) => ($(
-		fn $fn<F>(self, _v: $ty) -> Result<Self::Value, F>
-		where F: de::Error { Ok(Value::Invalid) }
-	)+);
-}
-
-impl<'de> de::Visitor<'de> for ValueVisitor<'de> {
-	type Value = Value<'de>;
-
-	fn expecting(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-		fmt.write_str("any value")
-	}
-
-	fn visit_u8<F>(self, value: u8) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Int(u32::from(value))) }
-
-	fn visit_u16<F>(self, value: u16) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Int(u32::from(value))) }
-
-	fn visit_u32<F>(self, value: u32) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Int(value)) }
-
-	fn visit_u64<F>(self, value: u64) -> Result<Self::Value, F>
-	where F: de::Error
-	{ Ok(Value::Int(u32::try_from(value).unwrap_or(Utc2k::MAX_UNIXTIME))) }
-
-	fn visit_str<F>(self, value: &str) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::String(value.into())) }
-
-	fn visit_borrowed_str<F>(self, value: &'de str) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Str(value)) }
-
-	fn visit_string<F>(self, value: String) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::String(value)) }
-
-	fn visit_bytes<F>(self, value: &[u8]) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::ByteBuf(value.to_vec())) }
-
-	fn visit_borrowed_bytes<F>(self, value: &'de [u8]) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Bytes(value)) }
-
-	fn visit_byte_buf<F>(self, value: Vec<u8>) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::ByteBuf(value)) }
-
-	invalid_type!(
-		(visit_bool, bool),
-		(visit_i8, i8),
-		(visit_i16, i16),
-		(visit_i32, i32),
-		(visit_i64, i64),
-		(visit_f32, f32),
-		(visit_f64, f64),
-		(visit_char, char)
-	);
-
-	fn visit_unit<F>(self) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Invalid) }
-
-	fn visit_none<F>(self) -> Result<Self::Value, F>
-	where F: de::Error { Ok(Value::Invalid) }
-
-	fn visit_some<D>(self, _deserializer: D) -> Result<Self::Value, D::Error>
-	where D: de::Deserializer<'de> { Ok(Value::Invalid) }
-
-	fn visit_newtype_struct<D>(self, _deserializer: D) -> Result<Self::Value, D::Error>
-	where D: de::Deserializer<'de> { Ok(Value::Invalid) }
-
-	fn visit_seq<V>(self, _visitor: V) -> Result<Self::Value, V::Error>
-	where V: de::SeqAccess<'de> { Ok(Value::Invalid) }
-
-	fn visit_map<V>(self, _visitor: V) -> Result<Self::Value, V::Error>
-	where V: de::MapAccess<'de> { Ok(Value::Invalid) }
-
-	fn visit_enum<V>(self, _visitor: V) -> Result<Self::Value, V::Error>
-	where V: de::EnumAccess<'de> { Ok(Value::Invalid) }
-}
+use std::fmt;
 
 
 
@@ -160,16 +24,7 @@ impl<'de> Deserialize<'de> for FmtUtc2k {
 	/// Use the optional `serde` crate feature to enable serialization support.
 	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
 	where D: de::Deserializer<'de> {
-		let raw = Value::deserialize(deserializer)?;
-		if let Some(v) = raw.as_str() {
-			Self::try_from(v).map_err(|_| de::Error::custom("invalid date string"))
-		}
-		else if let Some(v) = raw.as_int() {
-			Ok(Self::from(v))
-		}
-		else {
-			Err(de::Error::custom("expected timestamp or datetime string"))
-		}
+		Utc2k::deserialize(deserializer).map(Self::from)
 	}
 }
 
@@ -189,22 +44,80 @@ impl Serialize for FmtUtc2k {
 
 #[cfg_attr(docsrs, doc(cfg(feature = "serde")))]
 impl<'de> Deserialize<'de> for Utc2k {
-	#[allow(clippy::option_if_let_else)] // That looks bad here.
 	/// # Deserialize.
 	///
 	/// Use the optional `serde` crate feature to enable serialization support.
 	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
 	where D: de::Deserializer<'de> {
-		let raw = Value::deserialize(deserializer)?;
-		if let Some(v) = raw.as_int() {
-			Ok(Self::from(v))
+		struct Visitor;
+
+		macro_rules! invalid {
+			($fn:ident, $ty1:ty, $ty2:ident) => (
+				fn $fn<S>(self, src: $ty1) -> Result<Self::Value, S>
+				where S: de::Error {
+					Err(de::Error::invalid_type(
+						de::Unexpected::$ty2(src.into()),
+						&self
+					))
+				}
+			);
 		}
-		else if let Some(v) = raw.as_str() {
-			Self::try_from(v).map_err(|_| de::Error::custom("invalid date string"))
+
+		impl<'de> de::Visitor<'de> for Visitor {
+			type Value = Utc2k;
+
+			fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+				f.write_str("a timestamp or datetime string")
+			}
+
+			fn visit_str<S>(self, src: &str) -> Result<Self::Value, S>
+			where S: de::Error {
+				Utc2k::try_from(src).map_err(|_| de::Error::custom("invalid datetime string"))
+			}
+
+			fn visit_bytes<S>(self, src: &[u8]) -> Result<Self::Value, S>
+			where S: de::Error {
+				Utc2k::try_from(src).map_err(|_| de::Error::custom("invalid datetime string"))
+			}
+
+			fn visit_i32<S>(self, src: i32) -> Result<Self::Value, S>
+			where S: de::Error {
+				// Fail on negative, otherwise parse as usual.
+				u32::try_from(src)
+					.map(Utc2k::from)
+					.map_err(|_| de::Error::custom("invalid unix timestamp"))
+			}
+
+			fn visit_i64<S>(self, src: i64) -> Result<Self::Value, S>
+			where S: de::Error {
+				// Fail on negative, otherwise parse as usual.
+				u32::try_from(src)
+					.map(Utc2k::from)
+					.map_err(|_| de::Error::custom("invalid unix timestamp"))
+			}
+
+			fn visit_u32<S>(self, src: u32) -> Result<Self::Value, S>
+			where S: de::Error { Ok(Utc2k::from(src)) }
+
+			fn visit_u64<S>(self, src: u64) -> Result<Self::Value, S>
+			where S: de::Error {
+				// Return the max value on failure because it's too big,
+				// otherwise parse as normal.
+				Ok(u32::try_from(src).map_or_else(
+					|_| Utc2k::max(),
+					Utc2k::from,
+				))
+			}
+
+			// Too small to hold an in-range value.
+			invalid!(visit_char, char, Char);
+			invalid!(visit_i8, i8, Signed);
+			invalid!(visit_i16, i16, Signed);
+			invalid!(visit_u8, u8, Unsigned);
+			invalid!(visit_u16, u16, Unsigned);
 		}
-		else {
-			Err(de::Error::custom("expected timestamp or datetime string"))
-		}
+
+		deserializer.deserialize_any(Visitor)
 	}
 }
 
@@ -225,6 +138,12 @@ impl Serialize for Utc2k {
 #[cfg(test)]
 mod tests {
 	use super::*;
+
+	#[cfg(not(miri))]
+	const SAMPLE_SIZE: usize = 1_000_000;
+
+	#[cfg(miri)]
+	const SAMPLE_SIZE: usize = 1000; // Miri runs way too slow for a million tests.
 
 	#[test]
 	/// # Test Serialization.
@@ -265,6 +184,39 @@ mod tests {
 			date2 = serde_json::from_str(DATESTR_Q)
 				.expect("Utc2k deserialization (str) failed.");
 			assert_eq!(date, date2);
+		}
+	}
+
+	#[test]
+	fn t_serde_fmtutc2k_rng() {
+		let rng = fastrand::Rng::new();
+		for i in std::iter::repeat_with(|| rng.u32(Utc2k::MIN_UNIXTIME..=Utc2k::MAX_UNIXTIME)).take(SAMPLE_SIZE) {
+			let date = FmtUtc2k::from(i);
+
+			// Serialization should give us the date string with an extra pair
+			// of quotes.
+			let s = serde_json::to_string(&date).expect("Serialization failed.");
+			assert_eq!(format!("{:?}", date.as_str()), s);
+
+			// Deserialization should give us a copy of the original.
+			let d = serde_json::from_str::<FmtUtc2k>(&s).expect("Deserialization failed.");
+			assert_eq!(date, d);
+		}
+	}
+
+	#[test]
+	fn t_serde_utc2k_rng() {
+		let rng = fastrand::Rng::new();
+		for i in std::iter::repeat_with(|| rng.u32(Utc2k::MIN_UNIXTIME..=Utc2k::MAX_UNIXTIME)).take(SAMPLE_SIZE) {
+			let date = Utc2k::from(i);
+
+			// Serialization should give us the unixtime as a string.
+			let s = serde_json::to_string(&date).expect("Serialization failed.");
+			assert_eq!(s, i.to_string());
+
+			// Deserialization should give us a copy of the original.
+			let d = serde_json::from_str::<Utc2k>(&s).expect("Deserialization failed.");
+			assert_eq!(date, d);
 		}
 	}
 }
